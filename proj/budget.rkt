8463bb551501f17568039e62538bb2a5650a4a5e
Brandon Webster <brandon.webster@socrata.com>
2020-08-21 10:07:43 -0700

    bootstrap db with enum type

--- proj/budget.rkt
+++ proj/budget.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
-(require csv-reading)
+(require csv-reading
+         db)
 
 ;; CONSTANTS
 (define VENDOR_IDX 2)
@@ -12,6 +13,72 @@
     "person"
     "other"))
 (define VENDOR_CATEGORIES (make-hash))
+
+;; HELPERS
+(define (intersperse a xs)
+  (cond [(empty? xs) xs]
+        [(empty? (rest xs)) xs]
+        [else
+         (cons (first xs)
+               (cons a
+                (intersperse a (rest xs))))]))
+
+(define (list->sqlenum xs)
+  (string-append
+   "("
+   (foldl
+    (lambda (wrd acc)
+      (string-append wrd acc))
+    ""
+    (intersperse
+     ", "
+     (map
+      (lambda (x)
+        (string-append "'" x "'"))
+      xs)))
+   ")"))
+
+;; DATABASE
+(define pgdb
+  (postgresql-connect #:user "branweb"
+                      #:database "budget"))
+
+(define bootstrap?
+  (not (or (table-exists? pgdb "vendors")
+           (table-exists? pgdb "transactions"))))
+
+(define (bootstrap-tables)
+  (when bootstrap?
+    (let ([cat-query 
+           (string-append
+            "CREATE TYPE category AS ENUM "
+            (list->sqlenum CATEGORIES))]
+          [vendors-query
+           (string-append
+            "CREATE TABLE vendors ("
+            "id SERIAL PRIMARY KEY,"
+            "name varchar(255) NOT NULL,"
+            "category category NOT NULL)")]
+          [transactions-query
+           (string-append
+            "CREATE TABLE transactions ("
+            "id SERIAL PRIMARY KEY,"
+            "trans_amt numeric(6, 4) NOT NULL,"
+            "trans_date date NOT NULL,"
+            "vendor_id integer REFERENCES vendors(id))")])
+      (query-exec pgdb cat-query)
+      (query-exec pgdb vendors-query)
+      (query-exec pgdb transactions-query))))
+
+(define (insert-vendors)
+  (hash-for-each
+   VENDOR_CATEGORIES
+   (lambda (vendor cat)
+     (query-exec
+      pgdb
+      "INSERT INTO vendors (name, category) VALUES ($1, cast($2 as category))"
+      vendor
+      cat))))
 
 ;; PARSING
 (define (reader in)
@@ -54,6 +121,17 @@
         (list-ref CATEGORIES idx)
         (get-category vendor))))
 
+(define (categorize-vendors)
+  (for-each
+   (lambda (vendor)
+     (let ([cat (get-category vendor)])
+       (hash-set! VENDOR_CATEGORIES vendor cat)))
+;;   (get-unique-vendors (extract-vendors))
+   '("a" "b" "c")
+   ))
+
+
+
 ;; loads csv file
 ;; makes set of vendors therein
 ;; prompts user to categorize each vendor
@@ -62,24 +140,29 @@
 ;; needs to query db for each vendor
 ;; if unknown, then prompt user and add to hash
 ;; otherwise don't prompt
-;; when hash is build, use it to update the database
+;; when hash is build, use it to update the database (insert into vendors values (name, cat))
 ;; should either generate a report or add budget info to db
-(for-each
- (lambda (vendor)
-   (let ([cat (get-category vendor)])
-     (hash-set! VENDOR_CATEGORIES vendor cat)))
-;; (get-unique-vendors (extract-vendors))
- '("a" "b" "c")
- )
+;; (for-each
+;;  (lambda (vendor)
+;;    (let ([cat (get-category vendor)])
+;;      (hash-set! VENDOR_CATEGORIES vendor cat)))
+;; ;; (get-unique-vendors (extract-vendors))
+;;  '("a" "b" "c")
+;;  )
 
 ;; write V_C to database
 ;; make script with cmd line option for file...
 ;; or make csv reader read from std in and just use
 ;; shell redirects
 
-;; VENDORS (name text, cat_id int)
-;; CATEGORIES (name text)
-;; 1 CAT has many VENDORS
-;; consider making CATEGORY an enum type rather than a table
+
+(define (main)
+  (bootstrap-tables)
+  (categorize-vendors)
+  (insert-vendors)
+;;  (print VENDOR_CATEGORIES)
+  )
+
+(main)
 
 
