b420658d5fda4a85481d18c1eb1653d11af7eeb8
Brandon Webster <brandon.webster@socrata.com>
2020-08-21 14:02:19 -0700

    finished categorizing part of budget script

--- proj/budget.rkt
+++ proj/budget.rkt
@@ -12,31 +12,8 @@
     "home"
     "person"
     "other"))
+
 (define VENDOR_CATEGORIES (make-hash))
-
-;; HELPERS
-(define (intersperse a xs)
-  (cond [(empty? xs) xs]
-        [(empty? (rest xs)) xs]
-        [else
-         (cons (first xs)
-               (cons a
-                (intersperse a (rest xs))))]))
-
-(define (list->sqlenum xs)
-  (string-append
-   "("
-   (foldl
-    (lambda (wrd acc)
-      (string-append wrd acc))
-    ""
-    (intersperse
-     ", "
-     (map
-      (lambda (x)
-        (string-append "'" x "'"))
-      xs)))
-   ")"))
 
 ;; DATABASE
 (define pgdb
@@ -44,31 +21,46 @@
                       #:database "budget"))
 
 (define bootstrap?
-  (not (or (table-exists? pgdb "vendors")
-           (table-exists? pgdb "transactions"))))
+  (ormap
+   (lambda (t) (not (table-exists? pgdb t)))
+   (list "vendors" "categories" "transactions")))
 
-(define (bootstrap-tables)
+(define (create-tables)
+  (let ([cat-query 
+         (string-append
+          "CREATE TABLE categories("
+          "id SERIAL PRIMARY KEY,"
+          "name varchar(255) NOT NULL)")]
+        [vendors-query
+         (string-append
+          "CREATE TABLE vendors ("
+          "id SERIAL PRIMARY KEY,"
+          "name varchar(255) NOT NULL,"
+          "category_id integer REFERENCES categories(id))")]
+        [transactions-query
+         (string-append
+          "CREATE TABLE transactions ("
+          "id SERIAL PRIMARY KEY,"
+          "trans_amt numeric(6, 4) NOT NULL,"
+          "trans_date date NOT NULL,"
+          "vendor_id integer REFERENCES vendors(id))")])
+    (query-exec pgdb cat-query)
+    (query-exec pgdb vendors-query)
+    (query-exec pgdb transactions-query)))
+
+(define (insert-categories)
+  (for-each
+   (lambda (cat)
+     (query-exec
+      pgdb
+      "INSERT INTO categories (name) VALUES ($1)" cat))
+   CATEGORIES))
+
+(define (bootstrap-db)
   (when bootstrap?
-    (let ([cat-query 
-           (string-append
-            "CREATE TYPE category AS ENUM "
-            (list->sqlenum CATEGORIES))]
-          [vendors-query
-           (string-append
-            "CREATE TABLE vendors ("
-            "id SERIAL PRIMARY KEY,"
-            "name varchar(255) NOT NULL,"
-            "category category NOT NULL)")]
-          [transactions-query
-           (string-append
-            "CREATE TABLE transactions ("
-            "id SERIAL PRIMARY KEY,"
-            "trans_amt numeric(6, 4) NOT NULL,"
-            "trans_date date NOT NULL,"
-            "vendor_id integer REFERENCES vendors(id))")])
-      (query-exec pgdb cat-query)
-      (query-exec pgdb vendors-query)
-      (query-exec pgdb transactions-query))))
+    (display "Bootstrapping database...\n")
+    (create-tables)
+    (insert-categories)))
 
 (define (insert-vendors)
   (hash-for-each
@@ -76,20 +68,40 @@
    (lambda (vendor cat)
      (query-exec
       pgdb
-      "INSERT INTO vendors (name, category) VALUES ($1, cast($2 as category))"
+      "INSERT INTO vendors (name, category_id) VALUES ($1, $2)"
       vendor
-      cat))))
+      (query-value pgdb "SELECT id FROM categories WHERE name = $1" cat)))))
+
+(define known-vendors
+  (query-list pgdb "SELECT name FROM vendors"))
 
 ;; PARSING
 (define (reader in)
   (make-csv-reader in))
 
+;; convert rows to list of lists and remove headers
 (define rows
-  (reader (open-input-file "data.csv")))
+  (rest (csv->list (reader (open-input-file "data.csv")))))
+
+;; payments/credits show as postive amounts so filter them out
+(define (filter-payments xxs)
+  (filter
+   (lambda (xs)
+     (negative? (string->number (list-ref xs 5))))
+   xxs))
+
+(define (filter-known-vendors vendors)
+  (filter
+   (lambda (vendor)
+     (not (member vendor known-vendors)))
+   vendors))
 
 (define (extract-vendors)
-  (csv-map
-   (lambda (row) (list-ref row VENDOR_IDX)) rows))
+  (filter-known-vendors
+   (map
+    (lambda (row)
+      (list-ref row VENDOR_IDX))
+    (filter-payments rows))))
 
 (define (get-unique-vendors vendors)
   (cond [(empty? vendors) vendors]
@@ -101,7 +113,7 @@
 
 ;; CATEGORIZING
 (define (print-options opts)
-  (for ([idx (in-range (length opts))]
+  (for ([idx (in-range 1 (add1 (length opts)))]
         [cat opts])
     (display (string-append
               (number->string idx)
@@ -116,7 +128,7 @@
     vendor
     "\n"))
   (print-options CATEGORIES)
-  (let ([idx (string->number (read-line))])
+  (let ([idx (sub1 (string->number (read-line)))])
     (if (and (< idx (length CATEGORIES)) (>= idx 0))
         (list-ref CATEGORIES idx)
         (get-category vendor))))
@@ -126,42 +138,17 @@
    (lambda (vendor)
      (let ([cat (get-category vendor)])
        (hash-set! VENDOR_CATEGORIES vendor cat)))
-;;   (get-unique-vendors (extract-vendors))
-   '("a" "b" "c")
-   ))
+   (get-unique-vendors (extract-vendors))))
 
-
-
-;; loads csv file
-;; makes set of vendors therein
-;; prompts user to categorize each vendor
-;; builds a hash (VENDOR => CATEGORY) e.g. Chick-fil-a => food
-
-;; needs to query db for each vendor
-;; if unknown, then prompt user and add to hash
-;; otherwise don't prompt
-;; when hash is build, use it to update the database (insert into vendors values (name, cat))
-;; should either generate a report or add budget info to db
-;; (for-each
-;;  (lambda (vendor)
-;;    (let ([cat (get-category vendor)])
-;;      (hash-set! VENDOR_CATEGORIES vendor cat)))
-;; ;; (get-unique-vendors (extract-vendors))
-;;  '("a" "b" "c")
-;;  )
-
-;; write V_C to database
+;; categorize and add transactions to database
 ;; make script with cmd line option for file...
 ;; or make csv reader read from std in and just use
 ;; shell redirects
 
-
 (define (main)
-  (bootstrap-tables)
+  (bootstrap-db)
   (categorize-vendors)
-  (insert-vendors)
-;;  (print VENDOR_CATEGORIES)
-  )
+  (insert-vendors))
 
 (main)
 
